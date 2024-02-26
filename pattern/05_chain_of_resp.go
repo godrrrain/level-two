1ff145e874e4db2dc075616bf773b6cba3b22e3a
Vova <vova.degtyarev.01@mail.ru>
2024-02-26 18:56:47 +0300

    add 5-8 pattern

--- pattern/05_chain_of_resp.go
+++ pattern/05_chain_of_resp.go
@@ -1,7 +1,81 @@
 package pattern
 
 /*
-	Реализовать паттерн «цепочка вызовов».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
+Цепочка обязанностей является поведенческим паттерном проектирования уровня объекта, который
+позволяет передавать запросы последовательно по цепочке обработчиков.
+Каждый обработчик сам решает, стоит ли передавать запрос дальше по цепочке.
+
+Применение:
+	1. Хотим обрабатывать разнообразные запросы, заранее не зная, какие обработчики понадобятся.
+	2. Хотим динамически задавать набор объектов, способных обработать запрос.
+
+Плюсы:
+	1. Уменьшаем связанность между отправителем запроса и получателем.
+	2. Настраиваем обработку запросов более гибко.
+	2. Легко добавляем новые обработчики в систему.
+
+Минусы:
+	1. Запрос может остаться необработанным.
 */
+
+type Handler interface {
+	Handle(message int) string
+	SetNext(Handler) Handler
+}
+
+// handler "A".
+type ConcreteHandlerA struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerA) SendRequest(message int) (result string) {
+	if message == 1 {
+		result = "Im handler 1"
+	} else if h.next != nil {
+		result = h.next.Handle(message)
+	}
+	return
+}
+
+func (h *ConcreteHandlerA) SetNext(next Handler) Handler {
+	h.next = next
+	return next
+}
+
+// handler "B"
+type ConcreteHandlerB struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerB) SendRequest(message int) (result string) {
+	if message == 2 {
+		result = "Im handler 2"
+	} else if h.next != nil {
+		result = h.next.Handle(message)
+	}
+	return
+}
+
+func (h *ConcreteHandlerB) SetNext(next Handler) Handler {
+	h.next = next
+	return next
+}
+
+// handler "C"
+type ConcreteHandlerC struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerC) SendRequest(message int) (result string) {
+	if message == 3 {
+		result = "Im handler 3"
+	} else if h.next != nil {
+		result = h.next.Handle(message)
+	}
+	return
+}
+
+func (h *ConcreteHandlerC) SetNext(next Handler) Handler {
+	h.next = next
+	return next
+}
--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -1,7 +1,63 @@
 package pattern
 
+import "fmt"
+
 /*
-	Реализовать паттерн «фабричный метод».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Factory_method_pattern
+Фабричный метод является порождающим паттерном проектирования, который определяет общий интерфейс
+для создания объектов, позволяя подклассам изменять тип создаваемых объектов.
+
+Применение:
+	1. Хотим создавать различные объекты, не указывая конкретных типов объектов.
+
+Плюсы:
+	1. Избавляем главный класс от привязки к конкретным типам объектов.
+	2. Легко добавляем новые типы объектов в систему.
+
+Минусы:
+	1. Усложняем код внедрением дополнительных классов.
 */
+
+// Фабрика
+type LoggerFactory interface {
+	CreateLogger(action int) Logger
+}
+
+// Абстрактный класс логгера
+type Logger interface {
+	Log(message string)
+}
+
+type ConcreteLoggerFactory struct{}
+
+func NewCreator() LoggerFactory {
+	return &ConcreteLoggerFactory{}
+}
+
+func (f *ConcreteLoggerFactory) CreateLogger(action int) Logger {
+	var logger Logger
+
+	switch action {
+	case 1:
+		logger = &FileLogger{}
+	case 2:
+		logger = &ConsoleLogger{}
+	default:
+		fmt.Println("Unknown action")
+	}
+
+	return logger
+}
+
+// Файловый логгер
+type FileLogger struct{}
+
+func (logger *FileLogger) Log(message string) {
+	fmt.Println("Запись в файл:", message)
+}
+
+// Консольный логгер
+type ConsoleLogger struct{}
+
+func (logger *ConsoleLogger) Log(message string) {
+	fmt.Println("Вывод в консоль:", message)
+}
